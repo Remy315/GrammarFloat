af3ce557331c878bd0135450e2ece3b370d87368
user3284869005 <458910015700432-user3284869005@noreply.coze.cn>
2026-04-24 22:15:03 +0800

    fix: 使用 expo run:android 构建 APK

--- client/metro.config.js
+++ client/metro.config.js
@@ -1,7 +1,10 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withUniwindConfig } = require('uniwind/metro');
+const path = require('path');
 
-const config = getDefaultConfig(__dirname);
+// 确保使用 client 目录的路径
+const projectRoot = __dirname;
+const config = getDefaultConfig(projectRoot);
 const finalConfig = withUniwindConfig(config, { cssEntryFile: "./global.css" });
 
 module.exports = finalConfig;
